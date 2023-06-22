082aac0b8938251a3aefbc5e0795b02312a959bf
Yuki Watanabe <ukwhatn@gmail.com>
2023-06-22 15:59:31 +0900

    [install/setup] devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,4 @@
 class Admin < ApplicationRecord
+  # loginしか行わないので、database_authenticatableのみで良い
+  devise :database_authenticatable
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # -------------
   # アソシエーション
   # -------------
