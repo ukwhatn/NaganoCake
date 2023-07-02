41d2c66aa9e48a1c979b6dca87d9d9d09c8acc16
Yuki Watanabe <ukwhatn@gmail.com>
2023-07-02 21:57:03 +0900

    [add] ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,35 @@
 Rails.application.routes.draw do
+
+  root to: 'public/homes#top'
+
+  scope module: :public do
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'unsubscribe'
+        patch 'withdraw'
+      end
+    end
+    resources :cart_items, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post 'confirm'
+        get 'thanks'
+      end
+    end
+  end
+
+  namespace :admin do
+    get 'top' => 'homes#top'
+    resources :items, except: [:destroy]
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
